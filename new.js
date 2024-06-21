97ff83e302f151fff689d886ce06a150fabcf1d4
aygul2018 <teacher.aygunm@gmail.com>
2024-06-21 21:14:16 +0400

    fs

---
+++ new.js
@@ -0,0 +1,16 @@
+// let fullname = prompt("Your surname and Name");
+// console.log("Salam"+ " " + fullname) ;
+
+let num = 10;
+console.log( num>10);
+console.log(num>=10);
+console.log(num<=10);
+console.log(!num );
+console.log(!!num );
+
+let = 20;
+console.log( num>20);
+console.log(num>=20);
+console.log(num<=0);
+console.log(!num );
+console.log(!!!num )
